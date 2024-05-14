368dec3a7cc6ea6d6889be3a652e5f61fddf3a1f
bstriker <strikeforcezero@gmail.com>
2024-05-14 18:17:16 -0400

    feat: add manual plugin for applying the layer with other layers

--- src/plugin.rs
+++ src/plugin.rs
@@ -5,12 +5,16 @@ use crate::cache_system::cache_frame_count;
 use crate::config::FrameCountSubscriberConfig;
 use crate::subscriber_layer::add_log_plugin_with_custom_layer;
 
+fn init(app: &mut App) {
+    app.add_systems(First, cache_frame_count)
+        .init_resource::<FrameCountSubscriberConfig>();
+}
+
 pub struct FrameCountSubscriberPlugin;
 
 impl Plugin for FrameCountSubscriberPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(First, cache_frame_count)
-            .init_resource::<FrameCountSubscriberConfig>();
+        init(app);
         if !app.get_added_plugins::<LogPlugin>().is_empty() {
             panic!("LogPlugin already loaded, please disable with `.add_plugins(DefaultPlugins.build().disable::<LogPlugin>())` before loading FrameCountSubscriberPlugin, \
              or add the layer manually `LogPlugin {{ custom_layer: |app| Some(Box::new(vec![/* ..other layers here, */ bevy_frame_count_subscriber::subscriber::frame_count_layer(app))) }}`");
@@ -19,5 +23,13 @@ impl Plugin for FrameCountSubscriberPlugin {
             panic!("FrameCountSubscriberPlugin already loaded");
         }
         add_log_plugin_with_custom_layer(app);
+    }
+}
+
+pub struct FrameCountSubscriberPluginManual;
+
+impl Plugin for FrameCountSubscriberPluginManual {
+    fn build(&self, app: &mut App) {
+        init(app);
     }
 }
